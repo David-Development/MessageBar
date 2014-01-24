cd4177c7b29d0b6a65c5d4475f2edc10a2d9f81c
David-Development <david-dev@live.de>
2014-01-24 17:06:52 +0100

    Create android-library project for gradle

--- src/com/michaelflisar/messagebar/messages/ConstantMessage.java
+++
@@ -1,9 +0,0 @@
-package com.michaelflisar.messagebar.messages;
-
-public class ConstantMessage extends BaseMessage
-{
-    public ConstantMessage(String message, String button1Test)
-    {
-        super(message, button1Test, -1, null, -1, -1, false, null);
-    }
-}
